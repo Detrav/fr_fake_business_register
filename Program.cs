2e6f454b2f349262719dcfd60b054ffa7281208f
Witaly Ezepchuk <witaly@fast-report.com>
2019-04-01 16:17:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using FastReport;
+using System;
+using System.Collections.Generic;
+
+namespace fr_fake_business_register
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // step 1 prepare fake frx
+            SomeClass[] fakeList = new SomeClass[0];
+            Report r = new Report();
+            r.Dictionary.RegisterData(fakeList, "SomeTable", true);
+            r.Save("report.frx");
+
+            // step 2 on web
+
+            // REST via service xD
+            List<SomeClass> realDB = new List<SomeClass>();
+            for(int i =0; i< 100; i++)
+            {
+                realDB.Add(new SomeClass());
+            }
+            // end REST
+
+            Report report = new Report();
+            report.Load("report.frx");  
+            report.RegisterData(realDB, "SomeTable");
+
+            // show to console, but now only save prepared report
+            report.Prepare();
+            report.SavePrepared("report.fpx");
+        }
+    }
+}
---
+++ SomeClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fr_fake_business_register
+{
+    public class SomeClass
+    {
+        public SomeClass()
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = names[r.Next(names.Length)];
+            Tel = tels[r.Next(tels.Length)];
+        }
+
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+
+        private static Random r = new Random();
+        private static string[] names = @"Tod Kroeker
+Meggan Beauchamp
+Emiko Llanas
+Onita Carmack
+Ashlea Hales
+Nell Ashford
+Lorna Bungard
+Nada Cooperman
+Adolph Gephart
+Felicia Yaeger
+Cyrstal Buhl
+Donovan Kochan
+Evelin Aguinaga
+Catherina Replogle
+Danille Dicks
+Ian Rouillard
+Josiah Shetley
+Isobel Bellamy
+Janee Dessert
+Trey Zakrzewski".Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        private static string[] tels = @"+1-202-555-0197
++1-202-555-0155
++1-202-555-0134
++1-202-555-0108
++1-202-555-0110
++1-202-555-0195".Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
